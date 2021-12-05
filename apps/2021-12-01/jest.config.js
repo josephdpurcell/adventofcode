dbbd0bb78ca93dab6efe7ded5d63aca4a4d1ec30
Joseph D. Purcell <josephdpurcell@gmail.com>
2021-12-05 09:54:10 -0500

    Add day 1

---
+++ apps/2021-12-01/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: '2021-12-01',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/2021-12-01',
+};
---
+++ apps/2021-12-01/src/part1.ts
@@ -0,0 +1,4 @@
+import * as fs from 'fs';
+import * as os from 'os';
+
+export function part1(): void {}
