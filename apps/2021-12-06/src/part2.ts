0d348df6640b712c66d99d47a781ad05bc2c4143
Joseph D. Purcell <josephdpurcell@gmail.com>
2021-12-11 03:36:52 -0500

    I did it!

--- apps/2021-12-06/src/part2.ts
+++ apps/2021-12-06/src/part2.ts
@@ -56,24 +56,12 @@ function countFish(fish: Record<number, number>): number {
 export async function main(): Promise<void> {
   const population = getPopulation();
 
-  // const days = 256;
-  const days = 80;
-  
-  console.log(population);
+  const days = 256;
+  // const days = 80;
+
   const startCount = countFish(population);
-  let finalFish: Record<number, number> = {
-    0: 0,
-    1: 0,
-    2: 0,
-    3: 0,
-    4: 0,
-    5: 0,
-    6: 0,
-    7: 0,
-    8: 0,
-  };
+  let finalFish = population;
   for (let day = 0; day < days; day++) {
-    // console.log(`day ${day}`);
     const newFish: Record<number, number> = {
       0: 0,
       1: 0,
@@ -85,37 +73,26 @@ export async function main(): Promise<void> {
       7: 0,
       8: 0,
     };
-    for (const i in population) {
-    // Object.keys(population).forEach((value: string): void => {
-      const age = Number(i);
+    // console.log(`============================== ${day}`);
+    // console.log(finalFish);
+    for (let age = 0; age < 9; age++) {
+      const populationOfAge = finalFish[age] === undefined ? 0 : Number(finalFish[age]);
+      // console.log(`Age ${age}`);
       if (age === 0) {
-        newFish[6] = newFish[6] + population[i];
-        newFish[8] = newFish[8] + population[i];
+        newFish[6] = newFish[6] + populationOfAge;
+        newFish[8] = newFish[8] + populationOfAge;
+        // console.log(`    Adding to 6 = ${newFish[6]} + ${populationOfAge}`);
+        // console.log(`    Adding to 8 = ${newFish[8]} + ${populationOfAge}`);
       } else {
-        newFish[age - 1] = newFish[age - 1] + population[i];
+        const newAge = age - 1;
+        newFish[newAge] = newFish[newAge] + populationOfAge;
+        // console.log(`    Adding to ${newAge} = ${newFish[newAge]} + ${populationOfAge}`);
       }
     }
-    console.log(newFish);
     finalFish = newFish;
   }
-  console.log(finalFish);
-  // for (const age in population) {
-  //   if (age === 0) {
-  //     this.fish[i] = 6;
-  //     newFish.push(8);
-  //   } else {
-  //     this.fish[i]--;
-  //   }
-  // }
-  // const finalPopulation = population.getFish();
-  // const results = await Promise.all(promises);
-  // console.log(results);
-  // for (const result of results) {
-  //   for (const f of result) {
-  //     finalPopulation.push(f);
-  //   }
-  // }
-  // const endCount = finalPopulation.length;
+  // console.log('================================ DONE');
+  // console.log(finalFish);
   const endCount = countFish(finalFish);
 
   console.log(`We started with ${startCount} lanternfish at day 0`);
